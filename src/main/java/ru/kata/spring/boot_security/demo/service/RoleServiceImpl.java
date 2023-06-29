b4214a9f050417e77d9bce83031cde8e2937b722
simonsh030 <icesert75@gmail.com>
2023-06-29 10:27:32 +0300

    Jpa

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -1,39 +1,59 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.dao.RoleDao;
-import ru.kata.spring.boot_security.demo.model.Role;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.dao.RoleRepository;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
-    private final RoleDao roleDao;
+    private final RoleRepository roleRepository;
 
-    @Autowired
-    public RoleServiceImpl(RoleDao roleDao) {
-        this.roleDao = roleDao;
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
     }
 
-    @Transactional(readOnly = true)
     @Override
     public Set<Role> findRoles(List<Long> roles) {
-        return roleDao.findRoles(roles);
+        return roleRepository.findAllById(roles).stream().collect(Collectors.toSet());
     }
 
-    @Transactional(readOnly = true)
     @Override
     public List<Role> getAllRoles() {
-        return roleDao.getAllRoles();
+        return roleRepository.findAll();
     }
 
-    @Transactional
     @Override
     public void addDefaultRoles() {
-        roleDao.addDefaultRoles();
+        List<Long> roleIds = List.of(1L, 2L);
+
+        Set<Role> existingRoles = findRoles(roleIds);
+        boolean hasUserRole = false;
+        boolean hasAdminRole = false;
+
+        for (Role role : existingRoles) {
+            if (role.getName().equals("ROLE_USER")) {
+                hasUserRole = true;
+            }
+            if (role.getName().equals("ROLE_ADMIN")) {
+                hasAdminRole = true;
+            }
+        }
+
+        if (!hasUserRole) {
+            Role roleUser = new Role();
+            roleUser.setName("ROLE_USER");
+            roleRepository.save(roleUser);
+        }
+
+        if (!hasAdminRole) {
+            Role roleAdmin = new Role();
+            roleAdmin.setName("ROLE_ADMIN");
+            roleRepository.save(roleAdmin);
+        }
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,28 +1,26 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.kata.spring.boot_security.demo.dao.UserDao;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import ru.kata.spring.boot_security.demo.model.Role;
-import ru.kata.spring.boot_security.demo.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserRepository;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 import java.util.Set;
 
 @Service
-public class UserServiceImpl implements UserService, UserDetailsService{
+public class UserServiceImpl implements UserService, UserDetailsService {
 
-    private final UserDao userDao;
+    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public UserServiceImpl(UserDao userDao, PasswordEncoder passwordEncoder) {
-        this.userDao = userDao;
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -30,49 +28,52 @@ public class UserServiceImpl implements UserService, UserDetailsService{
     @Override
     public void addUser(User user, Set<Role> roles) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userDao.addUser(user, roles);
+        user.setRoles(roles);
+        userRepository.save(user);
     }
 
     @Transactional
     @Override
     public void deleteUserById(long id) {
-        userDao.deleteUserById(id);
+        userRepository.deleteById(id);
     }
 
     @Transactional
     @Override
     public User updateUser(User user, Set<Role> roles) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userDao.updateUser(user, roles);
+        user.setRoles(roles);
+        return userRepository.save(user);
     }
 
     @Transactional(readOnly = true)
     @Override
     public List<User> getAllUsers() {
-        return userDao.getAllUsers();
+        return userRepository.findAll();
     }
 
     @Transactional(readOnly = true)
     @Override
     public User findUserById(long id) {
-        return userDao.findUserById(id);
+        return userRepository.findById(id)
+                .orElse(null);
     }
 
     @Transactional(readOnly = true)
     @Override
     public User findUserByName(String name) {
-        return userDao.findUserByName(name);
+        return userRepository.findUserByName(name);
     }
 
     @Transactional(readOnly = true)
     @Override
     public boolean isUserNameUnique(String name) {
-        return userDao.isUserNameUnique(name);
+        return !userRepository.existsByName(name);
     }
 
     @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userDao.findUserByName(email);
+        return userRepository.findUserByName(email);
     }
 }
